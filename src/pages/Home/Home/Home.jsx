983ccf9ded3e3fe3135c4d4ced9f83159af1ef69
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-01-09 20:44:42 +0600

    added simple Top Banner, and react responsive carousel using awesome react components

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h2>Homeee</h2>
+            <Banner></Banner>
         </div>
     );
 };
