878b9765e58d099d0118d4ddfad970d5e16c8ab0
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-01-09 19:47:46 +0600

    Setup routes, layout and footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>hII</h1>
+      <h1>hII</h1> 
     </>
   )
 }
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Homeee</h2>
+        </div>
+    );
+};
+
+export default Home;
